52234dd3ebe5b87c38e02dff5ec6e41d89becb54
Rubylago <rubybibi6@gmail.com>
2022-06-24 12:06:54 +0800

    feat: user singup

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const attractionController = require('../controllers/attractionController')
+const userController = require('../controllers/userController')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
 router.get('/attractions', attractionController.getAttractions)
 
 // fallback 路由
-router.use('/', (req, res) => res.redirect('/attractions'))
+router.get('/', (req, res) => res.redirect('/attractions'))
 
 module.exports = router
