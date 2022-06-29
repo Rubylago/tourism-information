5a0d4fba30f3f355124f4fea60e4fd7555b36a7c
Rubylago <rubybibi6@gmail.com>
2022-06-29 12:39:55 +0800

    feat: create attraction

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -10,6 +10,32 @@ const adminController = {
     } catch (err) {
       next(err)
     }
+  },
+  createAttraction: async (req, res, next) => {
+    try {
+      res.render('admin/create-attraction')
+    } catch (err) {
+      next(err)
+    }
+  },
+  postAttraction: async (req, res, next) => {
+    try {
+      const { name, tel, introduction, photo, city, town, address } = req.body
+      if (!name) throw new Error('name is required')
+      await Attraction.create({
+        name,
+        tel,
+        introduction,
+        photo,
+        city,
+        town,
+        address
+      })
+      req.flash('success_messages', 'attraction successfully created')
+      res.redirect('/admin/attractions')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const adminController = require('../../controllers/adminController')
 
 router.get('/attractions', adminController.getAttractions)
+router.get('/attractions/create', adminController.createAttraction)
+router.post('/attractions', adminController.postAttraction)
 
 // fallback 路由
-router.get('/', (req, res) => res.redirect('/admin/attractions'))
+router.get('', (req, res) => res.redirect('/admin/attractions'))
 
 module.exports = router
