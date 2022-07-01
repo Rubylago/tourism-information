fae171661b44b1c800dbac0acab8107c6c652d36
Rubylago <rubybibi6@gmail.com>
2022-07-01 14:24:34 +0800

    feat: add attractions page

--- controllers/attractionController.js
+++ controllers/attractionController.js
@@ -1,6 +1,16 @@
+const { Attraction } = require('../models')
+
 const attractionController = {
-  getAttractions: (req, res) => {
-    return res.render('attractions')
+  getAttractions: async (req, res) => {
+    const attractions = await Attraction.findAll({
+      order: [['id', 'DESC']],
+      raw: true
+    })
+    const data = attractions.map(data => ({
+      ...data,
+      introduction: data.introduction.substring(0, 50)
+    }))
+    return res.render('attractions', { attractions: data })
   }
 }
 module.exports = attractionController
