732a50e2a0e985ca045b860cd24aac77e34fd499
Rubylago <rubybibi6@gmail.com>
2022-07-03 11:59:54 +0800

    feat: add comment model

--- models/attraction.js
+++ models/attraction.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      Attraction.hasMany(models.Comment, { foreignKey: 'attractionId' })
     }
   }
   Attraction.init({
